f047b47dee5ea37ff0c78b70e7d50da1ede8de7f
Matheus Rocha <matheus_rocha98@outlook.com>
2019-11-21 13:20:13 -0300

    Updates

---
+++ precache-manifest.45d3e17c77f02f65572991ccf169d00b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a84a27124b61965a3297e0e6f6214612",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "71c22c7ee0efb6064ea3",
+    "url": "/monsters-rolodex/static/css/main.d6e191c5.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "71c22c7ee0efb6064ea3",
+    "url": "/monsters-rolodex/static/js/main.aaf9231c.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
